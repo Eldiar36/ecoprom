53b534659a83d5f5f14510b7d7515493e6ebee73
eldiar36 <eldiarrd@gmail.com>
2021-01-19 01:26:06 +0600

    edit10

--- assets/js/script.js
+++ assets/js/script.js
@@ -125,25 +125,3 @@ for (const accordion of accordions) {
     }
 }
 
-$(document).ready(function(){
-    $(".nav-slider").owlCarousel({
-        items: 1,
-        loop:true,
-        nav:false,
-        dots:false,
-        margin:0,
-        responsive: {
-            0:{
-                items: 1,
-                margin:20,
-            },
-            480:{
-                items:2
-            },
-            767:{
-                items:1
-            },
-        }
-    });
-});
-
