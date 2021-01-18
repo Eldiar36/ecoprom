753ebf6f4a774ed8f677c6e16ceb71064456f8be
eldiar36 <eldiarrd@gmail.com>
2021-01-19 00:43:45 +0600

    edit9

--- assets/js/second.js
+++ assets/js/second.js
@@ -1,20 +1,4 @@
 
-        const accordions = document.querySelectorAll(".accordion-wrapper");
-        for (const accordion of accordions) {
-            const panels = accordion.querySelectorAll(".accordion");
-            for (const panel of panels) {
-                const head = panel.querySelector(".accordion-header");
-                head.addEventListener('click', () => {
-                    for (const otherPanel of panels) {
-                        if (otherPanel !== panel) {
-                            otherPanel.classList.remove('accordion-expanded');
-                        }
-                    }
-                    panel.classList.toggle('accordion-expanded');
-                });
-            }
-        }
-
         $(document).ready(function(){
             $(".nav-slider").owlCarousel({
                 items: 1,
